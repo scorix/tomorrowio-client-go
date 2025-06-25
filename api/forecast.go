2b63a476b4e0d34be87fc024be3267a7702a0ad0
scorix <zhangyu@caiyunapp.com>
2025-06-25 13:54:15 +0800

    fix: cache key with timestamp

--- api/forecast.go
+++ api/forecast.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"time"
 
 	lru "github.com/hashicorp/golang-lru"
 	"github.com/scorix/tomorrowio-client-go/cache"
@@ -28,12 +29,12 @@ func GetWeatherForecastURL(baseURL string, lat, lon float64, apiKey string) stri
 }
 
 func GetWeatherForecast(ctx context.Context, lruCache *lru.Cache, baseURL string, apiKey string, lat, lon float64) (*types.WeatherForecast, error) {
-	key := fmt.Sprintf("%f,%f", lat, lon)
+	cacheKey := fmt.Sprintf("%f,%f:%d", lat, lon, time.Now().Truncate(time.Hour).Unix())
 	fn := func() (*types.WeatherForecast, error) {
 		return getWeatherForecast(ctx, baseURL, apiKey, lat, lon)
 	}
 
-	return cache.WithLRU(lruCache, key, fn)
+	return cache.WithLRU(lruCache, cacheKey, fn)
 }
 
 func getWeatherForecast(ctx context.Context, baseURL string, apiKey string, lat, lon float64) (*types.WeatherForecast, error) {
